25b620695850bf9768c010de5d9be0ffcb597b60
joaovitorsta <joao.stanicheski@escola.pr.gov.br>
2025-06-06 11:44:45 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,8 +6,8 @@ criaCartao(
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Polonia?',
+    'A capital da Polonia é Varsóvia'
 )
 
 criaCartao(
@@ -18,6 +18,6 @@ criaCartao(
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz carro em Inglês?',
+    'Oi em ingles é Car'
 )
