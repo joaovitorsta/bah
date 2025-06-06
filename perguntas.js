7263f9e5266cef454e580f5dcb0d333e977ef46f
joaovitorsta <joao.stanicheski@escola.pr.gov.br>
2025-06-06 11:26:09 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
     'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Python é uma linguagem de programação.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a capital da França?',
+    'A capital da França é Paris.'
 )
 
 criaCartao(
     'Programação',
     'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Uma função é um bloco de código que executa uma tarefa.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Língua Inglesa',
+    'Como se diz "oi" em inglês?',
+    'Oi em inglês é "hi" (pronuncia-se "rai").'
 )
